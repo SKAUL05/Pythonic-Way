7a964a5b5bac1a7619a911f78b5330f480a5e4e5
SKAUL05 <kaul.sarath@gmail.com>
2022-03-01 21:45:45 +0530

    Changes Added

---
+++ design-patterns/Creational/AbstractFactory/autos/ford/lincoln.py
@@ -0,0 +1,10 @@
+from autos.abs_auto import AbsAuto
+
+class LincolnMKS(AbsAuto):
+    def start(self):
+        print("LincolnMKS Running smoothly")
+        
+    def stop(self):
+        print("LincolnMKS Shutting Down")
+        
+        
